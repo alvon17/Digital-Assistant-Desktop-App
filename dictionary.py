d1ee1d34b8f7ded9386d3d0062f90aa82543cb5f
alvon17 <73384433+alvon17@users.noreply.github.com>
2022-06-24 14:56:12 +0700

    update

--- dictionary.py
+++ dictionary.py
@@ -10,7 +10,7 @@ def check_words(query):
         temp = [(jaccard_distance(set(ngrams(word, 2)),
                                 set(ngrams(w, 2))),w)
                 for w in correct_words if w[0]==word[0]]
-        
+
         final_words.append(sorted(temp, key = lambda val:val[0])[0][1])
     final = ' '.join(final_words)
     return final
